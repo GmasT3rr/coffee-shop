decb76ab26f9b54fb1ad72269d77ee3ef9b48efa
Lucas Ramallo <lucramallo14@gmail.com>
2024-04-28 13:17:33 -0300

    Configurating global variables, folders, components & routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { LayoutComponent } from './layout/layout/layout.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [CommonModule, LayoutComponent],
+  template: `<app-layout></app-layout>`,
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
