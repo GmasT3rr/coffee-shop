b505061182008e98bf112c8ea3c39cdeb6377808
Lucas Ramallo <lucramallo14@gmail.com>
2024-04-28 14:13:46 -0300

    ADD: Navbar (res > 990px) WIP: responsive & anims

--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faUser } from '@fortawesome/free-regular-svg-icons';
+import { faSearch, faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
-
+  faSearch=faSearch
+  faUser=faUser
+  faShoppingCart=faShoppingCart
 }
