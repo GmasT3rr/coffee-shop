bb12386b5779372325b8524d755b0f6d2ef689e5
Lucas Ramallo <lucramallo14@gmail.com>
2024-05-02 00:44:16 -0300

    FIX: side menu, all toggled bug. FIX: nav-mobile first page loading margin error

--- src/app/layout/components/side-mobile-menu/side-mobile-menu.component.ts
+++ src/app/layout/components/side-mobile-menu/side-mobile-menu.component.ts
@@ -3,17 +3,21 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { ListItemComponent } from './components/list-item/list-item.component';
 import { SideMenuItem } from './interfaces/side-menu-list-item';
 import { ItemsService } from './services/items.service';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-side-mobile-menu',
   standalone: true,
-  imports: [CommonModule, ListItemComponent],
+  imports: [CommonModule, ListItemComponent,FontAwesomeModule],
   templateUrl: './side-mobile-menu.component.html',
   styleUrl: './side-mobile-menu.component.scss',
 })
 export class SideMobileMenuComponent implements OnInit {
   @Output() toggleMenuRequest = new EventEmitter<void>();
   @Input() showSideMenu: boolean = false;
+
+  fa=faXmark
 
   public toggleListButtonArray: boolean[] = [];
   public itemList: SideMenuItem[] = [];
